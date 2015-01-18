99e225147f54404130cf6304a8fb30478983cf5d
silwerowicz <pkisztelinski@gmail.com>
2015-01-18 12:55:46 +0100

    Adding an API endopint - urls.py

--- thinkster_django_angular_boilerplate/urls.py
+++ thinkster_django_angular_boilerplate/urls.py
@@ -2,8 +2,22 @@ from django.conf.urls import patterns, url
 
 from thinkster_django_angular_boilerplate.views import IndexView
 
+from rest_framework_nested import routers
+from authentication.views import AccountViewSet
+
+router = routers.SimpleRouter()
+router.register(r'accounts', AccountViewSet)
+
 urlpatterns = patterns(
     '',
 
+    url(r'^api/v1/', include(routers.urls)),
+
     url('^.*$', IndexView.as_view(), name='index'),
 )
+'''
+It is very important that the last URL in the above snippet always be the last URL.
+This is known as a passthrough or catch-all route. It accepts all requests not matched by a previous rule and passes the
+request through to AngularJS's router for processing. The order of other URLS is normally insignificant.
+
+'''
