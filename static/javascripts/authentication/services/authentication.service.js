c3882be56e3d555482399318c45862a2f1cffea9
silwerowicz <pkisztelinski@gmail.com>
2015-01-18 13:55:28 +0100

    added file authentication.service.js and create service for registering new users, add register.html file

---
+++ static/javascripts/authentication/services/authentication.service.js
@@ -0,0 +1,22 @@
+(function(){
+    'user strict';
+    angular.module('thinkster.authentication.services').factory('Authentication', Authentication);
+
+    Authentication.$inject = ['$cookies', '$http'];
+
+    function Authentication($cookies, $http){
+        var Authentication = {
+            register: register
+        };
+
+        return Authentication;
+
+        function register(email, password, username) {
+            return $http.post('/api/v1/accounts/', {
+                username: username,
+                password: password,
+                email: email
+            });
+        }
+    }
+})();
